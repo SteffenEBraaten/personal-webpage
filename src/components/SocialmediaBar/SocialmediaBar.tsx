c3a5baa82cf7dd85cbd391b4171228faaab8d641
SteffenEBraaten <steffen.e.braaten@gmail.com>
2020-12-17 15:44:10 +0100

    #12 Implemented social media bar

---
+++ src/components/SocialmediaBar/SocialmediaBar.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
+import styles from "./SocialMediaBar.module.css";
+
+const SocialMediaBar = () => {
+  return (
+    <ul className={styles.socialMediaBar}>
+      <li>
+        <a href="https://www.linkedin.com/in/steffen-ekeberg-braaten-942908156/">
+          <FontAwesomeIcon icon={["fab", "linkedin"]} />
+        </a>
+      </li>
+      <li>
+        <a href="https://github.com/SteffenEBraaten">
+          <FontAwesomeIcon icon={["fab", "github"]} />
+        </a>
+      </li>
+      <li>
+        <a href="mailto:steffen.e.braaten@gmail.com">
+          <FontAwesomeIcon icon={faEnvelope} />
+        </a>
+      </li>
+    </ul>
+  );
+};
+
+export default SocialMediaBar;
--- src/sections/Landing/Landing.tsx
+++ src/sections/Landing/Landing.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ProfilePicture from "../../components/ProfilePicture/ProfilePicture";
 import ProfileSteffen from "../../components/svgs/ProfileSteffen";
+import SocialMediaBar from "../../components/SocialMediaBar/SocialMediaBar";
 import styles from "./Landing.module.css";
 
 const Landing = () => {
@@ -11,6 +12,7 @@ const Landing = () => {
       <p className={styles.landingText}>
         I'm a software developer with a passion for great user experiences
       </p>
+      <SocialMediaBar />
     </section>
   );
 };
