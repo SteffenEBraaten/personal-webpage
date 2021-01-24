05aec3baa1524edc59b1726d5893ac10256df3d2
SteffenEBraaten <steffen.e.braaten@gmail.com>
2021-01-24 22:29:41 +0100

    #13 Set up initial skeleton for files

---
+++ src/components/Card/Card.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from "./Card.module.css";
+
+const Card = () => {
+  return (
+    <article>
+
+    </article>
+  );
+};
+
+export default Card;
---
+++ src/sections/WorkExperience/WorkExperience.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+import styles from "./WorkExperience.module.css";
+
+const WorkExperience = () => {
+  return (
+    <section>
+    </section>
+  );
+};
+
+export default WorkExperience;
