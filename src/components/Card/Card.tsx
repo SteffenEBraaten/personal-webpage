7c8af7baa8781eb42f1e6bd0aa4a92fc74703950
SteffenEBraaten <steffen.e.braaten@gmail.com>
2021-01-31 13:40:43 +0100

    #13 finished cards

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -2,19 +2,19 @@ import React from "react";
 import styles from "./Card.module.css";
 
 interface card {
-  title: string,
-  description: string,
-  timeperiod?: string,
-  image?: JSX.Element,
+  title: string;
+  description: string;
+  timeperiod?: string;
+  image?: JSX.Element;
 }
 
-const Card = ({title, description, timeperiod, image,}: card): JSX.Element => {
+const Card = ({ title, description, timeperiod, image }: card): JSX.Element => {
   return (
     <article className={styles.card}>
-        {image}
-        <h3>{title}</h3>
-        <p>{timeperiod}</p>
-        <p>{description}</p>
+      {image ? image : null}
+      <h3>{title}</h3>
+      {timeperiod ? <p>{timeperiod}</p> : null}
+      <p>{description}</p>
     </article>
   );
 };
--- src/sections/WorkExperience/WorkExperience.tsx
+++ src/sections/WorkExperience/WorkExperience.tsx
@@ -1,19 +1,47 @@
-import { title } from "process";
 import React from "react";
 import Card from "../../components/Card/Card";
+import SpareBank1Logo from "../../media/svg/SpareBank1Logo";
+import ProserviaLogo from "../../media/svg/ProserviaLogo";
 
 import styles from "./WorkExperience.module.css";
 
 const WorkExperience = () => {
   return (
-    <>
-      <h2>Work expereince</h2>
+    <section className={styles.workExperienceContainer}>
+      <h2 className={styles.centerH2}>Work expereince</h2>
       <section className={styles.cardList}>
-        <Card title={"SpareBank 1 Utvikling"} description={"description"} />
-        <Card title={"SpareBank 1 Utvikling"} description={"description"} />
-        <Card title={"Proservia ManpowerGroup"} description={"description"} />
+        <Card
+          title={"SpareBank 1 Utvikling"}
+          timeperiod={"Jan 2021 - Present"}
+          description={
+            "Handling incidents of critical IT-solutions as well as support to the banks in the evening."
+          }
+          image={<SpareBank1Logo />}
+        />
+        <Card
+          title={"SpareBank 1 Utvikling"}
+          timeperiod={"Jun 2020 - Aug 2020"}
+          description={
+            "Worked in agile teams and developed an web application with React and Typescript in the front end and Kotlin and Spring in the back end."
+          }
+          image={<SpareBank1Logo />}
+        />
+        <Card
+          title={"SpareBank 1 Utvikling"}
+          timeperiod={"Dec 2018 - Dec 2020"}
+          description={
+            "Handling incidents of critical IT-solutions as well as support to the banks in the evening."
+          }
+          image={<SpareBank1Logo />}
+        />
+        <Card
+          title={"Proservia ManpowerGroup"}
+          timeperiod={"Dec 2018 - Dec 2020"}
+          description={"Was a consultant rented out to SpareBank 1 Utvikling."}
+          image={<ProserviaLogo />}
+        />
       </section>
-    </>
+    </section>
   );
 };
 
