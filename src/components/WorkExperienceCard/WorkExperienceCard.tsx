a519afc96981a944a9cbcf07affff474fec0c0ad
Steffen Ekeberg Bråten <34305259+SteffenEBraaten@users.noreply.github.com>
2021-02-13 23:59:38 +0100

    Feature/#53 refactor workexperience card (#54)
    * #53 Refactored Work Experience Card
    
    * #53 Renamed folder
    
    * #53 Renamed folder

--- src/components/WorkExperienceCard/WorkExperienceCard.tsx
+++ src/components/WorkExperienceCard/WorkExperienceCard.tsx
@@ -1,24 +1,24 @@
 import React from "react";
-import styles from "./Card.module.css";
+import styles from "./WorkExperienceCard.module.css";
 
-interface card {
+interface WorkExperience {
   title: string;
   description: string;
-  timeperiod?: string;
-  image?: JSX.Element;
+  timeperiod: string;
+  image: JSX.Element;
 }
 
-const Card = ({ title, description, timeperiod, image }: card): JSX.Element => {
+const WorkExperienceCard = ({ title, description, timeperiod, image }: WorkExperience): JSX.Element => {
   return (
     // Need to add a dummy onClick method in order to make
     // touching the card in Safari iOS work properly
     <article className={styles.card} onClick={void(0)}>
-      {image ? image : null}
+      {image}
       <h3>{title}</h3>
-      {timeperiod ? <p>{timeperiod}</p> : null}
+      <p>{timeperiod}</p>
       <p>{description}</p>
     </article>
   );
 };
 
-export default Card;
+export default WorkExperienceCard;
