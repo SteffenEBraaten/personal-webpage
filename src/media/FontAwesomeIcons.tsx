7a29da93e466eb1d373d8037c9a2f2c27c9cebbc
Steffen Ekeberg Bråten <34305259+SteffenEBraaten@users.noreply.github.com>
2021-02-16 23:56:12 +0100

    #55 Refactored about me (#56)

--- src/media/FontAwesomeIcons.tsx
+++ src/media/FontAwesomeIcons.tsx
@@ -1,8 +1,15 @@
 import React from "react";
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
-import { faGlobe } from "@fortawesome/free-solid-svg-icons";
+import {
+  faEnvelope,
+  faGamepad,
+  faCamera,
+  faDumbbell,
+  faPlaneDeparture,
+  faBookOpen,
+  faGlobe,
+} from "@fortawesome/free-solid-svg-icons";
 
 /*
     A file to import and export different Font Awesome
@@ -10,9 +17,24 @@ import { faGlobe } from "@fortawesome/free-solid-svg-icons";
     individual file that needs them
 */
 
+const cameraIcon: JSX.Element = <FontAwesomeIcon icon={faCamera} />;
+const dumbellIcon: JSX.Element = <FontAwesomeIcon icon={faDumbbell} />;
+const gamingIcon: JSX.Element = <FontAwesomeIcon icon={faGamepad} />;
 const githubIcon = <FontAwesomeIcon icon={["fab", "github"]} />;
 const globeIcon = <FontAwesomeIcon icon={faGlobe} />;
 const linkedinIcon = <FontAwesomeIcon icon={["fab", "linkedin"]} />;
 const mailIcon = <FontAwesomeIcon icon={faEnvelope} />;
+const planeIcon: JSX.Element = <FontAwesomeIcon icon={faPlaneDeparture} />;
+const readingIcon: JSX.Element = <FontAwesomeIcon icon={faBookOpen} />;
 
-export { githubIcon, globeIcon, linkedinIcon, mailIcon };
+export {
+  cameraIcon,
+  dumbellIcon,
+  gamingIcon,
+  planeIcon,
+  readingIcon,
+  githubIcon,
+  globeIcon,
+  linkedinIcon,
+  mailIcon,
+};
--- src/sections/AboutMe/AboutMe.tsx
+++ src/sections/AboutMe/AboutMe.tsx
@@ -1,12 +1,11 @@
 import React from "react";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
-  faGamepad,
-  faCamera,
-  faDumbbell,
-  faPlaneDeparture,
-  faBookOpen,
-} from "@fortawesome/free-solid-svg-icons";
+  cameraIcon,
+  dumbellIcon,
+  gamingIcon,
+  planeIcon,
+  readingIcon,
+} from "../../media/FontAwesomeIcons";
 import profilePicture from "../../media/steffen.jpeg";
 
 import styles from "./AboutMe.module.css";
@@ -25,40 +24,38 @@ const ListItemWithIcon = ({ icon, text }: liWithIcon) => {
 };
 
 const AboutMe = () => {
-  const cameraIcon: JSX.Element = <FontAwesomeIcon icon={faCamera} />;
-  const dumbellIcon: JSX.Element = <FontAwesomeIcon icon={faDumbbell} />;
-  const gamingIcon: JSX.Element = <FontAwesomeIcon icon={faGamepad} />;
-  const planeIcon: JSX.Element = <FontAwesomeIcon icon={faPlaneDeparture} />;
-  const readingIcon: JSX.Element = <FontAwesomeIcon icon={faBookOpen} />;
-
   return (
-    <section className={styles.aboutMeSection}>
-      <article className={styles.profile}>
-        <h2 className={styles.h2Underline}>About me</h2>
-        <img
-          className={styles.profilePicture}
-          src={profilePicture}
-          alt="Steffen"
-        />
-        <p className={styles.profileText}>
-          I'm currently pursuing a masters degree in computer science at the
-          University of Oslo (UiO), while working part time as a operations
-          consultant.
-        </p>
-      </article>
-      <div className={styles.listRow}>
-        <div>
-          <h3>Some of my hobbies include</h3>
-          <ul className={styles.list}>
-            <ListItemWithIcon icon={gamingIcon} text={"Gaming"} />
-            <ListItemWithIcon icon={cameraIcon} text={"Photography"} />
-            <ListItemWithIcon icon={readingIcon} text={"Reading"} />
-            <ListItemWithIcon icon={dumbellIcon} text={"Training"} />
-            <ListItemWithIcon icon={planeIcon} text={"Travelling"} />
-          </ul>
+    <>
+      <section className={styles.aboutMeSection}>
+        <h2>About me</h2>
+        <div className={styles.aboutMeSectionContainer}>
+          <article className={styles.profile}>
+            <img
+              className={styles.profilePicture}
+              src={profilePicture}
+              alt="Steffen"
+            />
+            <p className={styles.profileText}>
+              I'm currently pursuing a masters degree in computer science at the
+              University of Oslo (UiO), while working part time as a operations
+              consultant.
+            </p>
+          </article>
+          <div className={styles.listRow}>
+            <div>
+              <h3>Some of my hobbies include</h3>
+              <ul className={styles.list}>
+                <ListItemWithIcon icon={gamingIcon} text={"Gaming"} />
+                <ListItemWithIcon icon={cameraIcon} text={"Photography"} />
+                <ListItemWithIcon icon={readingIcon} text={"Reading"} />
+                <ListItemWithIcon icon={dumbellIcon} text={"Training"} />
+                <ListItemWithIcon icon={planeIcon} text={"Travelling"} />
+              </ul>
+            </div>
+          </div>
         </div>
-      </div>
-    </section>
+      </section>
+    </>
   );
 };
 
